efb4a458b3fac46b96add2b4af3c3f2504793bcf
paik <jun@bigpearl.io>
2019-07-11 22:16:33 +0900

    helpme

--- picxi/views.py
+++ picxi/views.py
@@ -5,25 +5,34 @@ from .models import InPic, OutPic, SegPic
 from deep.humony_inference_url import *
 from rest_framework.views import APIView
 from config.settings import SERVERURL
+from django.http import JsonResponse
 class inpic(APIView):
     def post(self, request, format=None):
         i = InPic.objects.create(before = self.request.data["before"])
         cutimage = humony_segment(str("{0}{1}".format(SERVERURL, i.before)))
-        s = SegPic.objects.create(in_id = i ,ing = cutimage[1], color_list=cutimage[2])
-        response_data= "{\\r\\n       "+  "'before':"+ str("'{0}{1}'".format(SERVERURL, i.before)) +",\\r\\n         "+"'ing':"+ str("'{0}{1}'".format('http://127.0.0.1:8000/', s.ing)) + ",\\r\\n            " + "'color_list':"+ str(s.color_list) +"\\r\\n             }"
-        return Response(response_data, status=201)
+        s = SegPic.objects.create(in_id = i ,ing = cutimage[1][2:], color_list=cutimage[2])
+        return JsonResponse({
+        'before' : str("'{0}{1}'".format(SERVERURL, i.before)),
+        'ing' : str("'{0}{1}'".format('http://127.0.0.1:8000/', s.ing)),
+        'colo_list' : str(s.color_list),
+    }, json_dumps_params = {'ensure_ascii': True})
 
 
 
 class outpic(APIView):
     def post(self, request, format=None):
         i = self.request.data["before"]
-        i = i[:len(SERVERURL)]
+        i = i[len(SERVERURL):] 
+        i = "../"+i
         s = self.request.data["ing"]
-        s = s[:len(SERVERURL)]
+        s = s[len(SERVERURL):]
+        s = "../"+s
         cl = self.request.data["color_list"]
-        cs = self.request.data["color_sel"]        
-        r = humony_selcut(i, s, cl, cs)
-        OutPic(after=r, origin_id_id=id).save()
-        response_data = "{   \r    "+  "'after':"+ r.after +"   \n    }"
-        return Response(response_data, status=201)
+        cs = self.request.data["color_sel"]  
+        print(i)
+        print(s)      
+        r = humony_selcut(str(i), str(s), list(cl), list(cs))
+        OutPic.objects.create(after=r)
+        return JsonResponse({
+        'after' : str("'{0}{1}'".format(SERVERURL, r.after)),
+    }, json_dumps_params = {'ensure_ascii': True})
